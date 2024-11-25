3a2208fe3a1425c2b2234eeab32d6904faf70950
QuentinPace <quentinepace@gmail.com>
2024-11-25 12:20:24 -0500

    updatre usertask route completely done

--- app/api/user_tasks_routes.py
+++ app/api/user_tasks_routes.py
@@ -26,5 +26,4 @@ def updateUserTask (userTaskId) :
 
     db.session.commit()
 
-    
-    return make_response(jsonify({"usertaskId": userTaskId, "UserTask": user_task.to_dict_basic(), "query is_compled res": is_completed}), 200, {"Content-Type": "application/json"})
+    return make_response(jsonify({"message": "Successfully updated"}), 200, {"Content-Type": "application/json"})
