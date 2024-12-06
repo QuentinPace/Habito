b4a80846ddda9706078e9d91baaedf0f152a7d87
QuentinPace <quentinepace@gmail.com>
2024-12-05 19:31:10 -0500

    added isLoaded conditional to layout.jsx so every time any page is renbdered the user redux is there

--- react-vite/src/router/Layout.jsx
+++ react-vite/src/router/Layout.jsx
@@ -15,7 +15,7 @@ export default function Layout() {
   return (
     <>
       <ModalProvider>
-        <Navigation />
+        {isLoaded && <Navigation />}
         {isLoaded && <Outlet />}
         <Modal />
       </ModalProvider>
